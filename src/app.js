1cf4c2adb45fffb63bd0d288d872a131887762b5
EliezerRguez <eliezer.rguez93@gmail.com>
2021-07-28 18:36:40 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  document.getElementById("btn").addEventListener("click", () => {
+    document.getElementById("excuse").innerHTML = excuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let excuse = () => {
+  let pronombre = ["A", "The"];
+  let sujeto = ["dog", "cat", "driver", "mouse", "policeman"];
+  let accion = ["took my", "threw my", "ate my", "broke my", "stole my"];
+  let posesion = ["homework", "car", "toe", "money", "bike"];
+  let donde = [
+    "on the street",
+    "in my house",
+    "in my backyard",
+    "in my driveway"
+  ];
+
+  let proIndex = Math.floor(Math.random() * pronombre.length);
+  let sujetoIndex = Math.floor(Math.random() * sujeto.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let posesionIndex = Math.floor(Math.random() * posesion.length);
+  let dondeIndex = Math.floor(Math.random() * donde.length);
+
+  return (
+    pronombre[proIndex] +
+    " " +
+    sujeto[sujetoIndex] +
+    " " +
+    accion[accionIndex] +
+    " " +
+    posesion[posesionIndex] +
+    " " +
+    donde[dondeIndex]
+  );
 };
